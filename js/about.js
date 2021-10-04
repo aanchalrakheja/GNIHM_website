8f9e4df0a5dab612a51d7c61ca5970d2a459a19d
Salonee-Jain <salonijain1248@gmail.com>
2021-10-04 19:29:07 +0530

    added the about page

---
+++ js/about.js
@@ -0,0 +1,47 @@
+const toggleCollapse = document.querySelector(".toggle-collapse span");
+const nav = document.querySelector(".nav");
+// onclick event on toggle Collapse span tag
+toggleCollapse.onclick = (e) => {
+  nav.classList.toggle("collapse");
+  e.target.classList.toggle("toggle-click");
+};
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 20) {
+      $(".nav").addClass("sticky");
+    } else {
+      $(".nav").removeClass("sticky");
+    }
+  });
+});
+
+// JQUERY FOR OWL CAROUSEL
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 100,
+    nav: true,
+    dots: true,
+    navText: [
+      "<img class='customNextBtn' src='./assets/Facilities/leftarrow.png'/>",
+      "<img class='customPrevBtn' src='./assets/Facilities/rightarrow.png'/>",
+    ],
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        margin: 0,
+      },
+      550: {
+        items: 2,
+        margin: 10,
+      },
+      1000: {
+        items: 3,
+        dots: true,
+      },
+    },
+  });
+});
