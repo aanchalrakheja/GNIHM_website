db3c4e052fd79f92e242c799597a05e40f99d57f
Salonee-Jain <salonijain1248@gmail.com>
2021-10-05 16:13:29 +0530

    made the page responsive

--- js/about.js
+++ js/about.js
@@ -22,12 +22,8 @@ $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     loop: true,
     margin: 10,
-    nav: true,
-    dots: true,
-    navText: [
-      "<img class='customNextBtn' src='./assets/Facilities/leftarrow.png'/>",
-      "<img class='customPrevBtn' src='./assets/Facilities/rightarrow.png'/>",
-    ],
+    nav: false,
+    dots: false,
     responsiveClass: true,
     responsive: {
       0: {
@@ -44,3 +40,43 @@ $(document).ready(function () {
     },
   });
 });
+
+$('.owl-carousel.partner-image-div').owlCarousel({
+  center: true,
+  margin:10,
+  loop:true,
+  autoplay:true,
+  autoplayTimeout:3000,
+  responsive:{
+      0:{
+          items:1,
+          margin:1,
+
+      },
+      600:{
+          items:1,
+          margin:1,
+      },
+      1000:{
+          items:3,
+      }
+  }
+})
+
+$('#faculty').owlCarousel({
+  loop:true,
+  margin:10,
+ autoplay:true,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:4
+      }
+  }
+})
