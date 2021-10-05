7f9ca94a04351f2ab82a410406ae3b1f0bdaee08
ShrutikaGawas <shrutikagawas26@gmail.com>
2021-10-05 22:39:18 +0530

    home update

--- home/home.js
+++ home/home.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
 
 // JQUERY FOR OWL CAROUSEL
 $(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
+  $(".carousel-2").owlCarousel({
     loop: true,
     margin: 100,
     nav: true,
@@ -32,7 +32,7 @@ $(document).ready(function () {
     responsive: {
       0: {
         items: 1,
-        margin: 0,
+        margin: 100,
       },
       550: {
         items: 1,
@@ -41,6 +41,42 @@ $(document).ready(function () {
       1000: {
         items: 4,
         margin:35,
+        dots: false,
+        nav:false,
+        mouseDrag:false,
+      },
+    },
+  });
+});
+
+$(document).ready(function () {
+  $(".carousel-1").owlCarousel({
+    margin: 100,
+    nav: true,
+    dots: true,
+    navText: [
+      "<img class='customNextBtn' src='/assets/Facilities/leftarrow.png'/>",
+      "<img class='customPrevBtn' src='/assets/Facilities/rightarrow.png'/>",
+    ],
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        dots: true,
+        mouseDrag:true,
+        nav: false,
+        margin: 100,
+      },
+      550: {
+        items: 1,
+        dots: true,
+        nav: false,
+        mouseDrag:true,
+        margin: 24,
+      },
+      1000: {
+        items: 4,
+        margin:60,
         dots: false,
         nav:false,
         mouseDrag:false,
