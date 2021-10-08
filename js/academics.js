06433314790948c2725f98214c0384aaa91ac948
Dheeraj <gandhidheeraj9@gmail.com>
2021-10-08 18:16:59 +0530

    changed structure of file

--- js/academics.js
+++ js/academics.js
@@ -26,3 +26,21 @@ $(document).ready(function () {
     },
   });
 });
+const toggleCollapse = document.querySelector(".toggle-collapse span");
+const nav = document.querySelector(".nav");
+// onclick event on toggle Collapse span tag
+toggleCollapse.onclick = (e) => {
+  nav.classList.toggle("collapse");
+  e.target.classList.toggle("toggle-click");
+};
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 20) {
+      $(".nav").addClass("sticky");
+    } else {
+      $(".nav").removeClass("sticky");
+    }
+  });
+});
