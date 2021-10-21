1a01f95683b1372d1379ee7de596b64e952ed4f4
aanchalrakheja <aanchalrakheja2001@gmail.com>
2021-10-22 02:22:27 +0530

    gallery updated

--- js/gallery.js
+++ js/gallery.js
@@ -21,7 +21,7 @@ const videoGallery=document.getElementById("video-gallery");
 const imageGallery=document.getElementById("image-gallery");
 const imageButton=document.getElementById("image-button");
 const videoButton=document.getElementById("video-button");
-videoGallery.style.display="none";
+// videoGallery.style.display="none";
 
 function callImages(){
     imageButton.classList.add("active");
@@ -36,3 +36,21 @@ function callVideos(){
     videoGallery.style.display="flex";
     imageGallery.style.display="none"; 
 }
+
+const modal=document.getElementsByClassName("modal");
+const closeModal=document.getElementsByClassName("close-modal");
+const Images=document.getElementsByClassName("gallery-image");
+const modalImage=document.getElementById("modal-img");
+modal[0].style.display="none";
+
+closeModal[0].addEventListener("click",function(){
+  modal[0].style.display="none";
+});
+
+function imageModal(n)
+{
+  const currentImage=Images[n].src;
+  modalImage.src=currentImage;
+  modal[0].style.display="flex";
+}
+
